d5094225595cb02c45266f519c59fa2c93f78b25
chahyoil <chahyoil@gmail.com>
2024-08-25 15:30:18 +0900

    여러객체 한꺼번에 업데이트하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,42 @@
 import {useState} from 'react'
 import './App.css';
 
-export default function MovingDot() {
-  const [position, setPosition] = useState({x: 0, y: 0});
 
+export default function App() {
+  const [person, setPerson] = useState({
+    firstName: 'Barbara',
+    lastName: 'Hepworth',
+    email: 'bhepworth@sculpture.com',
+  });
+
+  function handleChange(e) {
+    
+    setPerson(person => ({...person, [e.target.name]: e.target.value}));
+  }
 
   return (
-    <div 
-      onPointerMove={(e) => {
-        setPosition({x: e.clientX, y: e.clientY});
-      }}
-    style={{position:'relative', height:'100vh', border:'2px solid blue'}}>
-      <div
-        style={{
-          position: 'absolute',
-          backgroundColor: 'red',
-          borderRadius: '50%',
-          transform: `translate(${position.x}px, ${position.y}px)`,
-          left: -10,
-          top: -10,
-          width: 20,
-          height: 20,
-        }}
-      />
-    </div>
-  )
+    <>
+      <div>
+        <label>
+          First name:
+          <input name='firstName' value={person.firstName} onChange={handleChange} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Last name:
+          <input name='lastName' value={person.lastName} onChange={handleChange} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Email:
+          <input name='email' value={person.email} onChange={handleChange} />
+        </label>
+      </div>
+      <p>
+        {person.firstName} {person.lastName} ({person.email})
+      </p>
+    </>
+  );
 }
