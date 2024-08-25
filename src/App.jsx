7d88605ba687d0e49a869cab36d503916b04ad33
chahyoil <chahyoil@gmail.com>
2024-08-25 11:32:45 +0900

    리스트 랜더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
 import './App.css';
-import Gallery from './components/Gallery.jsx';
-import CardProfile from './components/CardProfile.jsx';
+import { people } from './lib/data';
+import Person from './components/People';
 
-// 자바스크립트에서 false 판정
-// 0, '', null, undefined, NaN
+export default function List() {
+  // const listItems = people.map((person, index) => <li key={person}>{person + index}</li>);
+  
+  const chemists = people.filter((person) => person.profession === '화학자');
+  const notChemists = people.filter((person) => person.profession !== '화학자');
 
-function Item({name, isPacked}) {
-  // if(isPacked) {
-  //   return undefined;
-  // }
-  // return <li className='item'>✔{name}</li>
-
-  // return <li className={`item ${isPacked ? 'checked' : ''}`}>{isPacked ? name : name + '✔'}</li>
-
-  return <li className={`item ${isPacked ? 'checked' : ''}`}>
-    {/* {name} {isPacked && '✔'} */}
-    {name} {isPacked ? '✔' :'❌'}
-  </li>
-}
-
-export default function App() {
   return (
-    <section className="packing_list">
-      <h2>Sally Ride`s Packing List</h2>
-      <ul>
-        <Item isPacked={true} name='우주복'></Item>
-        <Item isPacked={true} name='황금잎이 달린 헷멧'></Item>
-        <Item isPacked={false} name='가족사진'></Item>
-      </ul>
+    <section className='list_wrap'>
+      <h2>과학자</h2>
+      <h3>화학자</h3>
+      <div className='list'>
+        {chemists.map((person) => (
+          Person(person)
+        ))}
+      </div>
+
+      {/* <hr></hr> */}
+      
+      <h3>다른 과학자</h3>
+      <div className='list'>
+      {notChemists.map((person) => (
+        Person(person)
+      ))}
+      </div>
     </section>
   )
 }
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,4 @@
 // utils.js
-export function getImageUrl(imageId, size = 's') {
-  return `https://i.imgur.com/${imageId}${size}.jpg`;
+export function getImageUrl(person, size = 's') {
+  return `https://i.imgur.com/${person.imageId}${size}.jpg`;
 }
