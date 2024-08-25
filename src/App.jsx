06d6f761cdf1b253e26ebd4943613f95d412b0c3
chahyoil <chahyoil@gmail.com>
2024-08-25 14:11:37 +0900

    useState 공부 시작

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
 import './App.css';
+import {useState} from 'react'
+import {sculptureList} from './lib/data'
 
-export default function LightSwitch() {
-  function handleChangeBG() {
-    const bodyStyle = document.body.style;
-    if(bodyStyle.backgroundColor  === 'black') {
-      bodyStyle.backgroundColor = 'white';
-    } else{
-      bodyStyle.backgroundColor = 'black';
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
+  function handleChangeIndex(change) {
+    if (sculptureList && sculptureList.length > 0 && index + change >= 0 && index + change < sculptureList.length) {
+      setIndex(index + change);
     }
   }
-  
+
+  function handleToggleDetails() {
+    setShowMore(!showMore);
+  }
+
+  const sculpture = sculptureList[index];
+
   return (
-    <button onClick={handleChangeBG}>Light Switch</button>
+    <div className='gallery'>
+      <button type='button' className='btn' onClick={() => {handleChangeIndex(1)}}>Next</button>
+      <button type='button' className='btn' onClick={() => {handleChangeIndex(-1)}}>Prev</button>
+      <h2>
+        <i>{sculpture.name}</i>
+        by {sculpture.artist}
+      </h2>
+      <div>({index + 1} of {sculptureList.length}) </div>
+      <div>
+        <img src={sculpture.url} alt={sculpture.alt}/>
+      </div>
+      <button type='button' onClick={handleToggleDetails}>
+        {showMore ? 'Hide' : 'Show'}
+      </button>
+      {showMore &&<p>{sculpture.description}</p>}
+    </div>
   )
 }
+
