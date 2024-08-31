975ebd3e91bb27e9d0ccb1e57a2462fb20faadc1
chahyoil <chahyoil@gmail.com>
2024-08-31 12:17:37 +0900

    같은 형제간 State 공유 예제

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,56 @@
 import './App.css';
 import { useState } from 'react';
 
-const initTodos = [
-  { id: 1, text: '할일 1' },
-  { id: 2, text: '할일 2' },
-  { id: 3, text: '할일 3' },
-  { id: 4, text: '할일 4' },
-];
+function Panel({title, isActive, children, onShow}) {
+  return (
+    <div className="panel">
+      <h3>{title}</h3>
+      {isActive ? <p>{children}</p> : <button type="button" onClick={onShow}>Show</button>}
+    </div>
+  )
+}
 
-/* 
-  배열 CRUD
-  - Create : [...기존 배열, 추가될 내용]
-  - Read   : map(), key에 고유값 저장
-  - Update : map() 돌리며 고유 id와 비교하여 업데이트
-  - Delete : filter()로 id와 일치하지 않는 요소로 새로운 배열을 생성
-*/
-
-export default function App() {
-  const [todos, setTodos] = useState(initTodos);
-
-  function handleAdd(idx) {
-    setTodos([
-      ...todos.slice(0,idx),
-      {id : Date.now(), text: '추가된 할일'},
-      ...todos.slice(idx),
-    ]);
-  }
+export default function Accordion() {
+  const [activeIndex, setActiveIndex] = useState(0);
 
   return (
-    <div>
-      <button type="button" onClick={() => {handleAdd(2)}}>특정위치에 추가</button>
-      {todos.map((todo) => (
-        <p key={todo.id}>{todo.text}</p>
-      ))}
+    <div className='accordion'>
+    <h2>Almaty, Kazakhstan</h2>
+      <Panel
+        title="About"
+        isActive={activeIndex === 0}
+        onShow={() => setActiveIndex(0)}
+        >
+        With a population of about 2 million, Almaty is Kazakhstan's largest
+        city. From 1929 to 1997, it was its capital city.
+      </Panel>
+      <Panel
+        title="Etymology"
+        isActive={activeIndex === 1}
+        onShow={() => setActiveIndex(1)}
+      >
+        The name comes from, the Kazakh word for "apple" and is often translated
+        as "full of apples". In fact, the region surrounding Almaty is thought
+        to be the ancestral home of the apple, and the wild Malus sieversii is
+        considered a likely candidate for the ancestor of the modern domestic
+        apple.
+      </Panel>
+      <Panel
+        title="Nonono"
+        isActive={activeIndex === 2}
+        onShow={() => setActiveIndex(2)}
+        >
+        With a population of about 2 million, Almaty is Kazakhstan's largest
+        city. From 1929 to 1997, it was its capital city.
+      </Panel>
+      <Panel
+        title="GoGoGo"
+        isActive={activeIndex === 3}
+        onShow={() => setActiveIndex(3)}
+        >
+        With a population of about 2 million, Almaty is Kazakhstan's largest
+        city. From 1929 to 1997, it was its capital city.
+      </Panel>
     </div>
   )
 }
