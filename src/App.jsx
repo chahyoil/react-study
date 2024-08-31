2eb20cedf683e5384efe5f7282b7bf98da7cefd7
chahyoil <chahyoil@gmail.com>
2024-08-31 10:20:47 +0900

    filter를 이용해서 삭제하는거 해봄

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,29 @@
-import { useState } from 'react';
 import './App.css';
-import { useImmer } from 'use-immer';
+import { useState } from 'react';
 
-export default function Form() {
-  const [person, setPerson] = useImmer({
-    name: 'Niki de Saint Phalle',
-    artwork: {
-      title: 'Blue Nana',
-      city: 'Hamburg',
-      image: 'https://i.imgur.com/Sd1AgUOm.jpg',
-    },
-  });
+const initialArtists = [
+  { id: 0, name: 'Marta Colvin Andrade' },
+  { id: 1, name: 'Lamidi Olonade Fakeye' },
+  { id: 2, name: 'Louise Nevelson' },
+];
 
-  function handleChange(e) {
-    if (e.target.name === 'name') {
-      setPerson((draft)=>{
-        draft.name = e.target.value;
-      });
-    } else {
-      setPerson((draft) => {
-        draft.artwork[e.target.name] = e.target.value;
-      });
-    }
-  }
+// 배열에 요소 추가
+export default function List() {
+  const [artists, setArtists] = useState(initialArtists);
 
   return (
-    <>
-      <div>
-        <label>
-          Name:
-          <input name="name" value={person.name} onChange={handleChange} />
-        </label>
-      </div>
-      <div>
-        <label>
-          Title:
-          <input name="title" value={person.artwork.title} onChange={handleChange} />
-        </label>
-      </div>
-      <div>
-        <label>
-          City:
-          <input name="city" value={person.artwork.city} onChange={handleChange} />
-        </label>
-      </div>
-      <div>
-        <label>
-          Image:
-          <input name="image" value={person.artwork.image} onChange={handleChange} />
-        </label>
-      </div>
-      <p>
-        <i>{person.artwork.title}</i>
-        {' by '}
-        {person.name}
-        <br />
-        (located in {person.artwork.city})
-      </p>
-      <img src={person.artwork.image} alt={person.artwork.title} />
-    </>
-  );
+    <div>
+      <h2>영감을 주는 조각가:</h2>
+      <ul>
+        {artists.map((artists) => (
+          <li key={artists.id}>
+            {artists.name}
+            <button type='button' onClick={() => {
+              setArtists(artists.filter((item) => item.id !== artists.id))}}>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
