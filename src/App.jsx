2ca3a1daeaa52cf6e71dcf4ce134282df8a8380c
chahyoil <chahyoil@gmail.com>
2024-08-31 14:47:08 +0900

    부모에서 세팅된 값을 자식에서 스테이트 관리

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,10 @@
 import './App.css';
-import {useReducer} from 'react';
-import {useState} from 'react';
+import AboutTab from './components/about/AboutTab';
 
-function MoneyReducer(state, action) {
-  action.payload = parseInt(action.payload) || 0;
-
-  switch (action.type) {
-    case 'deposit':
-      return state + action.payload;
-    case 'withdraw':
-      return state - action.payload;
-    default:
-      return state;
-  }
-}
-
-export default function Counter() {
-  const [money, dispatch] = useReducer(MoneyReducer, 0);
-  const [num, setNum] = useState(0);
-
+export default function App() {
   return (
-    <>
-      <div className="app">
-          <h2>useReducer 은행</h2>
-          <p>잔고 : {money}</p>
-          <input 
-            type="number" 
-            value={num} 
-            onChange={(e) => setNum(e.target.value)}
-            step={1000}
-            />
-      </div>
-
-      <button type='button' onClick={()=>dispatch({type:'deposit', payload:num})}>예금</button>
-      <button type='button' onClick={()=>dispatch({type:'withdraw', payload:num})}>인출</button>
-    </>
+    <div className='app'>
+      <AboutTab />
+    </div>
   )
 }
---
+++ src/components/TabContents.jsx
@@ -0,0 +1,12 @@
+export default function TabContents({tabs, activeTab}) {
+  return (
+    <div className="tab_contents">
+        {tabs.map((tab, index) => (
+            // activeTab === index && 
+            <div key={tab.menu} className={`content ${activeTab === index ? 'active' : ''}`}>
+                {tab.contents}    
+            </div>
+        ))}
+    </div>
+  )
+}
