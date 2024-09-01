bf42c9b66f72a6d1002ef7295fedcd60b2d8c27d
chahyoil <chahyoil@gmail.com>
2024-09-01 11:59:25 +0900

    useEffect를 비디오 실행 예제에 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
 import './App.css';
-import { useRef ,useState} from 'react';
+import { useRef ,useState, useEffect} from 'react';
 
-// 스톱워치
-export default function Form() {
-  const [isPlaying, setIsPlaying] = useState(false);
+function VideoPlayer({src, isPlaying}) {
   const videoRef = useRef(null);
 
-  function handleClick() {
-    setIsPlaying(!isPlaying);
-
-    if (isPlaying) {
-      videoRef.current.pause();
-    } else {
+  // useEffect는 return 이후에 실행이 된다.
+  // 외부에서 전달된 isPlaying의 값에 따라서 실행되어야 함.
+  // 의존성 배열에 값을 지정하면 지정된 값이 변경될때만 실행됨.
+  useEffect(() => {
+    if(isPlaying) {
       videoRef.current.play();
+    } else {
+      videoRef.current.pause();
     }
+  }, [isPlaying])
 
-  }
+  // 실행순서 기록
+  // 1) 컴포넌트 내 return. 2) useEffect 실행. 3) isPlaying state 값.
+  
 
-  return (
-    <>
-      <button onClick={handleClick}>
-        {isPlaying ? 'Stop' : 'Start'}
-      </button>
-      <video width="250" ref={videoRef}>
-        <source
-          src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-          type="video/mp4"
-        />
-      </video>
-    </>
-  )
+  return <video src={src} ref={videoRef} className="video" loop playsInline></video>
 }
 
+export default function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <div>
+        <div>{count}</div>
+        <button onClick={()=>{setCount(count + 1)}}>+1</button>
+      </div>
+      <button onClick={() => {setIsPlaying(!isPlaying)}}>
+        {isPlaying ? 'pause' : 'play'}
+      </button>
+      <VideoPlayer src="https://www.w3schools.com/html/mov_bbb.mp4" isPlaying={isPlaying}/>
+    </div>
+  )
+}
