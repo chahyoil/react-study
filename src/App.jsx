6b48a5b6a3d099c2e46a758cf010f8f8453110f6
chahyoil <chahyoil@gmail.com>
2024-09-01 12:12:18 +0900

    useEffect에서 함수를 cleanup 하는 예제

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,32 @@
 import './App.css';
 import { useRef ,useState, useEffect} from 'react';
 
-function VideoPlayer({src, isPlaying}) {
-  const videoRef = useRef(null);
+function Test() {
 
-  // useEffect는 return 이후에 실행이 된다.
-  // 외부에서 전달된 isPlaying의 값에 따라서 실행되어야 함.
-  // 의존성 배열에 값을 지정하면 지정된 값이 변경될때만 실행됨.
   useEffect(() => {
-    if(isPlaying) {
-      videoRef.current.play();
-    } else {
-      videoRef.current.pause();
+    function handleScroll() {
+      console.log(window.scrollY);
     }
-  }, [isPlaying])
+    window.addEventListener('scroll', handleScroll);
 
-  // 실행순서 기록
-  // 1) 컴포넌트 내 return. 2) useEffect 실행. 3) isPlaying state 값.
-  
+    // cleanup function
+    // 함수 안에서 리턴된 함수를 '클로저'
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, []);
 
-  return <video src={src} ref={videoRef} className="video" loop playsInline></video>
+  return <h2>Test</h2>
 }
 
 export default function App() {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [count, setCount] = useState(0);
-
+  const [isShow, setIsShow] = useState(true);
   return (
-    <div>
-      <div>
-        <div>{count}</div>
-        <button onClick={()=>{setCount(count + 1)}}>+1</button>
-      </div>
-      <button onClick={() => {setIsPlaying(!isPlaying)}}>
-        {isPlaying ? 'pause' : 'play'}
+    <div className='app'>
+      <button type='button' onClick={() => setIsShow(!isShow)}>
+        테스트컴포넌트 토글
       </button>
-      <VideoPlayer src="https://www.w3schools.com/html/mov_bbb.mp4" isPlaying={isPlaying}/>
+      {isShow && <Test />}
     </div>
   )
 }
